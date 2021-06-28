ba10d29e8073990451adb49c0b95155aefa06fd7
Ray Espinosa <ray.esro@gmail.com>
2021-06-28 10:11:45 -0500

    Tests for tax round up calculation were added

---
+++ Core/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Core
+{
+    public class Product
+    {
+        public decimal ShelfPrice { get; private set; }
+       
+        public Product( decimal shelfPrice)
+        {
+            ShelfPrice = shelfPrice;
+        }
+
+        public decimal RoundUpTaxes(decimal tax)
+        {
+            var saleTax = ShelfPrice * tax;
+
+            decimal decimalPart = (int)((saleTax - (int)saleTax) * 100); ;
+
+            if (decimalPart % 10 >= 5)
+                saleTax = (int)(saleTax) + (Math.Ceiling(decimalPart / 10) / 10);
+            else if (decimalPart % 10 < 5)
+                saleTax = (int)(saleTax) + (Math.Floor(decimalPart / 10) / 10);
+
+            return Math.Round(saleTax, 2);
+
+        }
+    }
+}
---
+++ CoreTests/DecimalRoundUpTest.cs
@@ -0,0 +1,35 @@
+﻿using Core;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace CoreTests
+{
+    public class DecimalRoundUpTest
+    {
+        public static IEnumerable<TestCaseData> RoundUpTaxProducts()
+        {
+            //Ceil
+            yield return new TestCaseData(new Product(14.99m), 1.5m);
+            yield return new TestCaseData(new Product(5.60m), 0.6m);
+            yield return new TestCaseData(new Product(106.60m), 10.7m);
+            //Floor
+            yield return new TestCaseData(new Product(4.49m), 0.4m);
+            yield return new TestCaseData(new Product(53.25m), 5.3m);
+            yield return new TestCaseData(new Product(221.40m), 22.1m);
+            yield return new TestCaseData(new Product(25.50m), 2.6m);
+
+        }
+
+        [Test]
+        [TestCaseSource(nameof(RoundUpTaxProducts))]
+        public void Given_ATaxForAProduct_When_RoundUpTheSaleTax_Then_SaleTaxIsRoundedUpToNearest5Cents(Product product, decimal expectedTax)
+        {
+            //Arranged
+            var tax = 0.1m;
+            //Act
+            var roundesUpTax = product.RoundUpTaxes(tax);
+            //Assert
+            Assert.AreEqual(expectedTax, roundesUpTax);
+        }
+    }
+}
