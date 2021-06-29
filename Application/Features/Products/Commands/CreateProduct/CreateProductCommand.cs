1208ee8b4fd3103de843407032a621bc70ead400
Ray Espinosa <ray.esro@gmail.com>
2021-06-28 20:58:05 -0500

    -Added a command to create a product -Added a Response class which will be use on commands and queries -Added a class for tests the CreateProoductCommand class

---
+++ Application/Features/Products/Commands/CreateProduct/CreateProductCommand.cs
@@ -0,0 +1,43 @@
+﻿using Application.Interface.Services;
+using Application.Interfaces.Repositories;
+using Application.Wrappers;
+using Domain.Entities;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Features.Products.Commands.CreateProduct
+{
+    public class CreateProductCommand : IRequest<Response<int>>
+    {
+        public string Name { get; }
+        public decimal ShelfPrice { get; }
+
+        public CreateProductCommand(string name, decimal shelfPrice)
+        {
+            Name = name;
+            ShelfPrice = shelfPrice;
+        }
+    }
+
+    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, Response<int>>
+    {
+        private readonly IShoppingCartRepository _shoppingCartRepository;
+        private readonly ITaxingService _taxingService;
+
+        public CreateProductCommandHandler(IShoppingCartRepository shoppingCartRepository, ITaxingService taxingService)
+        {
+            _shoppingCartRepository = shoppingCartRepository;
+            _taxingService = taxingService;
+        }
+
+        public async Task<Response<int>> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = new Product(request.Name, request.ShelfPrice);
+            _taxingService.AssignTaxesTo(product);
+            _taxingService.CalculateTaxesFor(product);
+            await _shoppingCartRepository.AddProductAsync(product);
+            return new Response<int>(1);
+        }
+    }
+}
---
+++ Application/Wrappers/Response.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Wrappers
+{
+    public class Response<T>
+    {
+        public List<string> Errors { get; set; }
+        public bool Succeeded =>  Errors.Count == 0;
+
+        public T Data { get; set; }
+        public Response()
+        {
+            Errors = new List<string>();
+        }
+        public Response(T data) 
+        {
+            Errors = new List<string>();
+            Data = data;
+        }
+        public void AddError(string error)
+        {
+            Errors.Add(error);
+        }
+       
+    }
+}
